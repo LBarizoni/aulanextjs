6c690e85c12cc26d2e46add48748f4ae40b27ce3
LUIGI DOS SANTOS BARIZONI DE ALMEIDA <luigi.almeida01>
2026-02-27 11:05:23 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
- function Home() {
+const Card = () => {
+  return (
+    <div>
+      <h3>Aula introdução</h3>
+      <h3>Aula Estilização</h3>
+      <h3>Aula Componentes</h3>
+    </div>
+  );
+}
+
+function Home() {
   return (
     <div>
       <h1>React com Next JS</h1>
@@ -9,4 +19,5 @@ import styles from "./page.module.css";
   );
 }
 
-export default Home
+
+export default Home;
